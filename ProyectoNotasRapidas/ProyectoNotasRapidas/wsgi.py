9b57ab93ecb57159d78743d8199b7eef125fb531
PauloHumano <111820621+PauloHumano@users.noreply.github.com>
2022-12-03 21:07:10 -0300

    correccion de nombres buenas practicas  instalacion del proyecto django

--- ProyectoNotasRapidas/ProyectoNotasRapidas/wsgi.py
+++ ProyectoNotasRapidas/ProyectoNotasRapidas/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AppDjangoNotasRapidas project.
+WSGI config for ProyectoNotasRapidas project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppDjangoNotasRapidas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoNotasRapidas.settings')
 
 application = get_wsgi_application()
